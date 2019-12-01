dc1a81b537bb238acaed79a8d6d81eba2c1aeec2
root <mol81mol@yandex.ru>
2019-12-01 10:01:47 +0300

    Arguments added to the project.

--- subdomainfinder.py
+++ subdomainfinder.py
@@ -1,23 +1,51 @@
 import requests
 from socket import gethostbyname
+from sys import argv
 
-domain = input('domain: ')
-wordlist = input('wordlist: ')
-protocol = input('protocol: ')
+def help():
+    print('--domain (domain name to scan)')
+    print('--wordlist (list of words to search)')
+    quit()
 
 def generator(string):
     for word in string:
         subdomain = word.replace('\n','')
         yield subdomain
 
-def request(domain,protocol):
+def request(domain):
     try:
-        requests.get(protocol + domain)
+        ip = gethostbyname(domain)
     except:
         pass
     else:
-        print(domain + "     " + gethostbyname(domain))
+        print(domain + "     " + ip)
+
+
+
+if '--help' in argv:
+    help()
+
+if '--domain' in argv:
+    domain = argv[argv.index('--domain') + 1]
+else:
+    print('****************************************')
+    print('specify the argument --domain')
+    print('****************************************')
+    help()
+
+if '--wordlist' in argv:
+    wordlist = argv[argv.index('--wordlist') + 1]
+else:
+    print('****************************************')
+    print('specify the argument --wordlist')
+    print('****************************************')
+    help()
+
+
+
+
+
 
 with open(wordlist,'rt') as dictionary:
     for subdomain in generator(dictionary):
-        request(subdomain + '.' + domain,protocol)
+        request(subdomain + '.' + domain)
